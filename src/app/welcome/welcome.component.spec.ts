c9f8478e7c483c4c7935f0005dedef6ae4898d52
Katie Kunesh <kkunesh2020@chadwickschool.org>
2019-07-17 15:33:11 -0700

    refactor: move group select to home page and add tests for dropdown

--- src/app/welcome/welcome.component.spec.ts
+++ src/app/welcome/welcome.component.spec.ts
@@ -1,25 +1,87 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
-// import { WelcomeComponent } from './welcome.component';
+import { WelcomeComponent } from './welcome.component';
+import { Observable, of } from 'rxjs';
+import TestUtils from '../shared/utils/test-utils';
+import { MaterialModule } from '../shared/material.module';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GroupService } from '../services/groups.service';
+import { DebugElement } from '@angular/core';
+import { SelectMenuTestHelper } from '../shared/utils/select-menu-helper';
+import { AppRoutingModule } from '../app-routing.module';
+import { TodayWorkoutComponent } from '../workout/today.workout.component';
+import { AdminComponent } from '../admin/admin.component';
 
-// describe('WelcomeComponent', () => {
-//   let component: WelcomeComponent;
-//   let fixture: ComponentFixture<WelcomeComponent>;
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+  let componentDebug: DebugElement;
+  let componentElement: HTMLElement;
+  let options: HTMLElement[];
+  let selectMenu: SelectMenuTestHelper;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ WelcomeComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  const groupServiceStub = {
+    getAddedGroups(): Observable<any> {
+      return of([TestUtils.getTestGroup(
+        'basketball',
+        undefined
+      ), TestUtils.getTestGroup(
+        'waterpolo',
+        undefined
+      )]);
+    }
+  };
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(WelcomeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [MaterialModule, FormsModule, NoopAnimationsModule, AppRoutingModule],
+      declarations: [WelcomeComponent, TodayWorkoutComponent, AdminComponent],
+      providers: [
+        {
+          provide: GroupService,
+          useValue: groupServiceStub
+        }
+      ]
+    }).compileComponents();
+  }));
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeComponent);
+    component = fixture.componentInstance;
+    componentDebug = fixture.debugElement;
+    componentElement = componentDebug.nativeElement;
+    fixture.detectChanges();
+    selectMenu = new SelectMenuTestHelper(fixture);
+  });
+
+  beforeEach(fakeAsync(() => {
+    selectMenu.triggerMenu();
+    options = selectMenu.getOptions();
+  }));
+
+  afterEach(() => {
+    selectMenu.cleanup();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display home', () => {
+    const d = componentElement.querySelector('h1');
+    expect(d.textContent).toContain('Home');
+  });
+
+  it('should display the dropdown', () => {
+    const d = componentElement.querySelector('mat-form-field');
+    expect(d.textContent).toContain('Select Your Group');
+  });
+
+  it('should display groups correctly in the dropdown', () => {
+    const basketballElement = selectMenu.getOptionByKey(options, 'basketball');
+    const waterpoloElement = selectMenu.getOptionByKey(options, 'waterpolo');
+    expect(basketballElement).not.toBeNull();
+    expect(waterpoloElement).not.toBeNull();
+  });
+});
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Group } from '../shared/models/group.model';
+import { GroupService } from '../services/groups.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'wla-welcome',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  groupDataSource: Group;
+  constructor(private groupService: GroupService, private router: Router) { }
 
-  constructor() { }
+  group = {
+    name: '',
+    id: ''
+  };
 
   ngOnInit() {
+    this.showGroups();
   }
 
+  showGroups() {
+    this.groupService.getAddedGroups().subscribe(group => {
+      this.groupDataSource = group;
+    });
+  }
+
+  // seeWorkout() {
+  //   this.router.navigate(['today-workout-student']);
+  // }
 }
