3fadbee6ccc041de547dc0b0969e3a211d3b7d40
Jacob Dulai <jdulai2020@chadwickschool.org>
2019-07-18 10:21:41 -0700

    fix: Fixed routing url not updating

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@
 // import {WelcomeComponent} from './welcome/welcome.component';
 // import {TodayWorkoutComponent} from './workout/today.workout.component';
 // import { AdminComponent } from './admin/admin.component';
+// import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 // const routes: Routes = [
 //   {path: '', component: WelcomeComponent},
 //   {path: 'today-workout-student', component: TodayWorkoutComponent},
-//   {path: 'today-workout-admin', component: AdminComponent}
+//   {path: 'today-workout-admin', component: AdminComponent},
+//   {path: 'admin-home', component: AdminHomeComponent}
 // ];
 
 // @NgModule({
