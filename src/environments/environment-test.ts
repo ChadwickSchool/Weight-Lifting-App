c4f3efb0fd639b1740c45993203915c6693dfb1a
Jacob Dulai <jdulai2020@chadwickschool.org>
2019-08-09 11:28:42 -0700

    feat: Add Test Firestore Database

---
+++ src/environments/environment-test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD7trwNDxzlXch_lxp_CRug8H3pD3o6SeQ',
+    authDomain: 'test-weight-lifting-app-26052.firebaseapp.com',
+    databaseURL: 'https://test-weight-lifting-app-26052.firebaseio.com',
+    projectId: 'test-weight-lifting-app-26052',
+    storageBucket: '',
+    messagingSenderId: '12372520831',
+    appId: '1:12372520831:web:c1cf4f754dba6654'
+  }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,9 +9,9 @@ export const environment = {
     authDomain: 'workout-app-a6fdb.firebaseapp.com',
     databaseURL: 'https://workout-app-a6fdb.firebaseio.com',
     projectId: 'workout-app-a6fdb',
-    storageBucket: '',
+    storageBucket: 'workout-app-a6fdb.appspot.com',
     messagingSenderId: '88970154207',
-    appId: '1:88970154207:web:12fe1eb41f05cc15'
+    appId: '1:88970154207:web:0be40fb9b0c0f995'
   }
 };
 
