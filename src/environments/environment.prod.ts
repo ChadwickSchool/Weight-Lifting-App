055b5d8600651b426c3bda9d506291dd78d7a7c8
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2019-10-15 10:15:15 -0700

    Set up Pivotal Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCdMpbnOX2kT87GmhEPzbKz2UZQT8EEemo',
-    authDomain: 'test-app-c16d4.firebaseapp.com',
-    databaseURL: 'https://test-app-c16d4.firebaseio.com',
-    projectId: 'test-app-c16d4',
-    storageBucket: 'test-app-c16d4.appspot.com',
-    messagingSenderId: '532731549820',
-    appId: '1:532731549820:web:4a6bd3e81ea8b1f0'
+    apiKey: 'AIzaSyAVxtvlTEfuVedihUDtUz4YK6ZtThhftCs',
+    authDomain: 'workout-app-a6fdb.firebaseapp.com',
+    databaseURL: 'https://workout-app-a6fdb.firebaseio.com',
+    projectId: 'workout-app-a6fdb',
+    storageBucket: 'workout-app-a6fdb.appspot.com',
+    messagingSenderId: '88970154207',
+    appId: '1:88970154207:web:0be40fb9b0c0f995'
   }
 };
 
